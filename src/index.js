779deb486b8ed699f9510ae3c29a10b17c440a1f
ch1llc0d3 <chillcrib7@gmail.com>
2024-06-03 00:29:33 -0400

    weather app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-function handleSearchSubmit(event) {
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+  
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+  }
+  
+  function searchCity(city) {
+    let apiKey = "4b8t3575997a0fbfef8539da07c4o512";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
+
+  function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    // call the API
-    // search for the city
+  
+    searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Paris");
